52cff64566be2b910f941ef1e5b67c63fe5d598d
ymayroferreira-trafilea <ymayro.ferreira@trafilea.com>
2026-03-29 16:27:49 -0300

    feat: atualizacao apra micro front end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,10 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
       import('./components/login/login.component').then(m => m.LoginComponent)
-  },
-  {
-    path: 'register',
-    loadComponent: () =>
-      import('./components/register/register.component').then(m => m.RegisterComponent)
-  },
-  {
-    path: 'dashboard',
-    loadComponent: () =>
-      import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'change-password',
-    loadComponent: () =>
-      import('./components/change-password/change-password.component').then(m => m.ChangePasswordComponent),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'settings',
-    loadComponent: () =>
-      import('./components/user-settings/user-settings.component').then(m => m.UserSettingsComponent),
-    canActivate: [authGuard]
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
